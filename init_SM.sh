3b34b0bb82f213b622a67efbee2ca999c67dc8fb
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-10-09 16:18:54 +0100

    Tried -qq

--- init_SM.sh
+++ init_SM.sh
@@ -4,10 +4,10 @@
 
 
 # Check for available updates
-sudo apt-get update
+sudo apt-get -qq update
 
 # Make sure the certificate handler and curl utilities are installed
-sudo apt-get install ca-certificates curl -y
+sudo apt-get -qq install ca-certificates curl -y
 
 # Modern install method: add Docker's official GPG key:
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -19,7 +19,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get -qq update
 
 # Install the latest version of some Docker packages
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
