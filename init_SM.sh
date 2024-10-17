901c57d79f84b8d9ac64682a60bc705465d9e06d
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-10-17 13:17:54 +0100

    Bugfix/apt get (#5)
    * Swapped apt to apt-get for CLI stability. apt is UI focused and doesn't parse well.
    
    * -qq to reduce terminal noise
    
    * Echo docker + compose versions instead of auto output
    
    * Added a few stage echos

--- init_SM.sh
+++ init_SM.sh
@@ -4,10 +4,10 @@
 
 
 # Check for available updates
-sudo apt update
+sudo apt-get -qq update
 
 # Make sure the certificate handler and curl utilities are installed
-sudo apt install ca-certificates curl -y
+sudo apt-get -qq install ca-certificates curl -y
 
 # Modern install method: add Docker's official GPG key:
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -19,20 +19,30 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+sudo apt-get -qq update
 
 # Install the latest version of some Docker packages
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+echo "        Installing docker packages..."
+sudo apt-get -qq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
 # Lets the user account run Docker without elevated privileges
+echo "        Adding current user to docker user group..."
 sudo usermod -a -G docker $USER
 
 # Starts Docker running in the background
+echo "        Starting docker now and on every boot..."
 sudo systemctl start docker
 
 # Sets Docker to run in the background whenever the board starts up
 sudo systemctl enable docker
 
-# Note: the user now needs to log out and in again (or reboot) 
+# Test the install by printing the version numbers
+echo -n "        Installed "
+docker -v
+echo -n "        Installed "
+docker compose version
+
+# Note: the user now needs to log out and in again (or reboot)
 # for the permission changes above to take effect
+echo "        User permissions will be applied after the next reboot"
